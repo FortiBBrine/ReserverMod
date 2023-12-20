fad40fedca740c41881c7f5923301c87511a7a99
Aleksandr Steblevets <fortibrinelike@gmail.com>
2023-12-20 20:58:36 +0200

    видалив кажанів

--- src/main/java/reservermod/event/ModEventBusServerEvents.java
+++ src/main/java/reservermod/event/ModEventBusServerEvents.java
@@ -1,9 +1,9 @@
 package reservermod.event;
 
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.entity.EntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.server.ServerStartedEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import reservermod.ReserverMod;
@@ -15,6 +15,11 @@ public class ModEventBusServerEvents {
     public void onStartServer(ServerStartedEvent event) {
         MinecraftServer server = event.getServer();
 
+        server.getAllLevels().forEach(level -> level.getAllEntities().forEach(entity -> {
+            if (entity.getType() == EntityType.BAT) {
+                entity.kill();
+            }
+        }));
     }
 
 }
